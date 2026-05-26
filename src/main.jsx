7075120b2edd6e14485470e540fb9ab1db5ad4ba
alessandrorr1007 <alessandrorr1007@gmail.com>
2026-05-26 01:07:47 -0500

    fix: inicializar mercado pago en frontend

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { initMercadoPago } from "@mercadopago/sdk-react";
+
 import App from "./App.jsx";
 import "./style.css";
 import { AuthProvider } from "./context/AuthContext.jsx";
+
+initMercadoPago(import.meta.env.VITE_MP_PUBLIC_KEY, {
+  locale: "es-PE",
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
